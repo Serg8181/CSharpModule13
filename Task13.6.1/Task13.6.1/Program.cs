e50e7a8a667ee1626c3938ab794a88a42d5c72ea
Serg8181 <147308625+Serg8181@users.noreply.github.com>
2024-05-24 14:14:21 +0300

    Module13. Task1 Task2

---
+++ Task13.6.1/Task13.6.1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task13._6._1
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            string path = @"Text1.txt";
+            
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+               
+            double count1 = 0.00000;    
+            string str;
+            List<string> list = new List<string>(); 
+            using (StreamReader reader = new StreamReader(path))
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                while ((str = reader.ReadLine()) != null)
+                {
+                   
+                    list.Add(str);
+                   
+                   
+                }
+                sw.Stop();
+                count1 = sw.Elapsed.TotalMilliseconds;
+                
+            }
+
+            Console.WriteLine($"Производительность вставки в List<T>: {count1} мс ");
+            Console.WriteLine();
+
+            double count2 = 0.00000;
+            string str2;
+            LinkedList<string> list2 = new LinkedList<string>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                Stopwatch sw2 = new Stopwatch();
+                sw2.Start();
+                while ((str2 = reader.ReadLine()) != null)
+                {
+                   
+                    list2.AddLast(str2);
+                   
+                }
+                sw2.Stop();
+                count2 = sw2.Elapsed.TotalMilliseconds;
+               
+            }
+           
+            Console.WriteLine($"Производительность вставки в LinkedList<T>: {count2} мс ");
+            Console.ReadKey();
+
+        }
+       
+    }
+
+}
---
+++ Task13.6.2/Task13.6.2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task13._6._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"Text1.txt";
+            
+
+            //проверяем наличие файла
+            if(!File.Exists(path))
+            {
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+            
+            string noPunctuationText;
+            using (StreamReader sr = new StreamReader(path))
+            {        
+                //считываем из файла текст, отбрасываем знаки пунктуации
+                noPunctuationText = new string(sr.ReadToEnd().Where(c => !char.IsPunctuation(c)).ToArray());
+                
+            }
+            //разбиваем строку с текстом на массив слов
+            char[] sep = {' ', '\r','\n' };
+            var words = noPunctuationText.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+
+            //формируем массив не повторяющихся слов 
+            var keys = new HashSet<string>(words);
+
+            //формируем словарь с ключами и нулевыми значениями
+            Dictionary<string,int> commonWords = new Dictionary<string,int>();
+            foreach (string word in keys) 
+            {
+                commonWords.Add(word,0);
+            }
+
+            foreach (string word in words)
+            {
+                //проходим по массиву слов и при совпадении с ключем словаря, увеличиваем  значение из словаря на единицу
+               if (commonWords.ContainsKey(word)) commonWords[word]++;               
+            }
+            //сортируем словарь по значению
+            var result = commonWords.OrderBy(x => x.Value).ToList();
+            result.Reverse();
+            //выводим в консоль 10 слов 
+            Console.WriteLine("Эти 10 слов чаще всего встречаются в тексте: ");
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine( result[i].Key + "    " + result[i].Value );
+            }
+            
+         Console.ReadKey();
+        }
+    }
+}
